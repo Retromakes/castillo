fc62c034373e6f38d81b0e3d754a0125d53dd685
Retromakes <yaumito@gmail.com>
2025-03-06 13:12:09 +0100

    warp to level FUNCIONA del 1 al 2

--- dev/loader/loadercpc.asm
+++ dev/loader/loadercpc.asm
@@ -30,13 +30,13 @@ org $BA00
 
 ; Load binary
 
-	ld      ix, $BA00 - 13375
-	ld      de, 13375
+	ld      ix, $BA00 - 13406
+	ld      de, 13406
 	call    cpct_miniload_asm
 
 ; Depack binary
 
-	ld      hl, $BA00 - 13375
+	ld      hl, $BA00 - 13406
 	ld      de, $0400
 	call    dzx7_standard
 
--- dev/my/ci/extra_routines.h
+++ dev/my/ci/extra_routines.h
@@ -4,10 +4,25 @@
 switch (n_pant) {
     // nariz
     case 27:
-        if (p_tx == 8 && p_ty == 5) {
+        if (p_tx == 8) {
             // reproduzco sonido
             wyz_play_sound(3);
+            
+    level = 1;
+    n_pant = 24;
+    gpx = 1 << 4; p_x = gpx << FIXBITS;
+    gpy = 5 << 4; p_y = gpy << FIXBITS;
+    warp_to_level = success = 1;
+    playing = 0;
 
+/*
+playing = 0;
+    n_pant = 26;
+    gpx = 5 << 4; p_x = gpx << FIXBITS;
+    gpy = 7 << 4; p_y = gpy << FIXBITS;
+//    warp_to_level = success = 1;
+success = 1;
+*/
 /*
 // suelto el objeto
 flags[30] = 0;
